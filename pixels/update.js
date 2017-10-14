6d2fc4ec5ec5874de32185496c99252c38af5444
bauwen <bauwen.demol@hotmail.com>
2017-10-14 11:26:46 +0200

    more pixels

--- pixels/update.js
+++ pixels/update.js
@@ -6,12 +6,12 @@ function update() {
     
     var s = Math.floor(x + y) / 10;
     
-    setcolor(0, 255, 255);
+    setcolor(0, 255, 55);
     rectfill(10, 20, 20, 100);
     
     rectfill(180, 140, 30, 30);
     
     setcolor(255, 128, 0);
     
-    for (var i = 0; i < 1; i++) circleFill(x, y, s); //rectfill(x, y, s, s);
+    for (var i = 0; i < 100; i++) circleFill(x, y, s); //rectfill(x, y, s, s);
 }
