c935b1c3607aaa77d7286f1f2814d21a44ccb992
bauwen <bauwen.demol@hotmail.com>
2017-10-14 15:17:35 +0200

    pixel controls

--- pixels/update.js
+++ pixels/update.js
@@ -1,17 +1,26 @@
 function update() {
-    x += (keyboard[RIGHT] - keyboard[LEFT]) * 4;
-    y += (keyboard[DOWN] - keyboard[UP]) * 4;
+    x += (keyboard[RIGHT] - keyboard[LEFT]) * 3;
+    y += (keyboard[DOWN] - keyboard[UP]) * 3;
     
     //for (var i = 0; i < 1; i++) circleFill(x, y, (x + y) / 10);
     
     var s = Math.floor(x + y) / 10;
     
-    setcolor(0, 255, 55);
-    rectfill(10, 20, 20, 100);
+    if (keyboard[SPACE]) {
+        setcolor(200, 200, 200);
+        rectfill(0, 0, 192, 160);
+    }
     
+    if (!keyboard[ENTER]) {
+        setcolor(0, 255, 55);
+    } else {
+        setcolor(255, 0, 0);
+    }
+    
+    rectfill(10, 20, 20, 100);
     rectfill(180, 140, 30, 30);
     
     setcolor(255, 128, 0);
     
-    for (var i = 0; i < 30; i++) circleFill(x, y, s); //rectfill(x, y, s, s);
+    for (var i = 0; i < 10; i++) circleFill(x, y, s); //rectfill(x, y, s, s);
 }
