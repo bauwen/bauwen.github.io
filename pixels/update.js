67ee6f4f76223ea55e9896c90b74228d348f1dc2
bauwen <bauwen.demol@hotmail.com>
2017-10-14 11:24:00 +0200

    pixels

---
+++ pixels/update.js
@@ -0,0 +1,17 @@
+function update() {
+    x += (keyboard[RIGHT] - keyboard[LEFT]) * 4;
+    y += (keyboard[DOWN] - keyboard[UP]) * 4;
+    
+    //for (var i = 0; i < 1; i++) circleFill(x, y, (x + y) / 10);
+    
+    var s = Math.floor(x + y) / 10;
+    
+    setcolor(0, 255, 255);
+    rectfill(10, 20, 20, 100);
+    
+    rectfill(180, 140, 30, 30);
+    
+    setcolor(255, 128, 0);
+    
+    for (var i = 0; i < 1; i++) circleFill(x, y, s); //rectfill(x, y, s, s);
+}
