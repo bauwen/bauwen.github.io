423ce63d9d32ddd5a242bcab03eb431d26efe272
bauwen <bauwen.demol@hotmail.com>
2017-10-15 18:44:22 +0200

    other music

--- pixels/music.js
+++ pixels/music.js
@@ -1,21 +1,531 @@
-var synth = new Tone.PolySynth(8).toMaster()
 
-MidiConvert.load("audio/song.mid", function(midi) {
+var startAudio = function() {
+    $scope = window;
+    
+    $scope.tempo = 180;
 
-  // make sure you set the tempo before you schedule the events
-  console.log(midi.tracks.length);
-  Tone.Transport.bpm.value = midi.header.bpm
+    var conductor = new BandJS();
 
-  for (var i = 0; i < midi.tracks.length; i++) {
-      // pass in the note events from one of the tracks as the second argument to Tone.Part 
-      var midiPart = new Tone.Part(function(time, note) {
+    conductor.setTimeSignature(2, 2);
+    conductor.setTempo($scope.tempo);
 
-        //use the events to play the synth
-        synth.triggerAttackRelease(note.name, note.duration, time, note.velocity)
+    var rightHand = conductor.createInstrument('square', 'oscillators'),
+        leftHand = conductor.createInstrument('triangle', 'oscillators'),
+        drum = conductor.createInstrument('white', 'noises');
 
-      }, midi.tracks[i].notes).start()
-  }
+    drum.setVolume(50);
 
-  // start the transport to hear the events
-  Tone.Transport.start()
-});
+    /**
+     * Intro
+     */
+        // Bar 1
+    rightHand.note('quarter', 'E5, F#4')
+        .note('quarter', 'E5, F#4')
+        .rest('quarter')
+        .note('quarter', 'E5, F#4');
+
+    leftHand.note('quarter', 'D3')
+        .note('quarter', 'D3')
+        .rest('quarter')
+        .note('quarter', 'D3');
+
+    drum.rest('whole');
+
+    // Bar2
+    rightHand.rest('quarter')
+        .note('quarter', 'C5, F#4')
+        .note('quarter', 'E5, F#4')
+        .rest('quarter');
+
+    leftHand.rest('quarter')
+        .note('quarter', 'D3')
+        .note('quarter', 'D3')
+        .rest('quarter');
+
+    drum.rest('whole');
+
+    // Bar 3
+    rightHand.note('quarter', 'G5, B4, G4')
+        .rest('quarter')
+        .rest('half');
+
+    leftHand.rest('whole');
+
+    drum.rest('whole');
+
+    // Bar 4
+    rightHand.note('quarter', 'G4')
+        .rest('quarter')
+        .rest('half');
+
+    leftHand.note('quarter', 'G3')
+        .rest('quarter')
+        .rest('half');
+
+    drum.rest('whole');
+
+    // Bar 5/13
+    rightHand.repeatStart()
+        .note('quarter', 'C5, E4')
+        .rest('quarter')
+        .rest('quarter')
+        .note('quarter', 'G4, C4');
+
+    leftHand.repeatStart()
+        .note('quarter', 'G3')
+        .rest('quarter')
+        .rest('quarter')
+        .note('quarter', 'E3');
+
+    drum.repeatStart()
+        .note('eighth', 'A4')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter', 'A4')
+        .rest('tripletQuarter')
+        .note('tripletQuarter', 'A4');
+
+    // bar 6/14
+    rightHand.rest('half')
+        .note('quarter', 'E4, G3')
+        .rest('quarter');
+
+    leftHand.rest('half')
+        .note('quarter', 'C3')
+        .rest('quarter');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 7/15
+    rightHand.rest('quarter')
+        .note('quarter', 'A4, C4')
+        .rest('quarter')
+        .note('quarter', 'B4, D4');
+
+    leftHand.rest('quarter')
+        .note('quarter', 'F3')
+        .rest('quarter')
+        .note('quarter', 'G3');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 8/16
+    rightHand.rest('quarter')
+        .note('quarter', 'Bb4, Db4')
+        .note('quarter', 'A4, C4')
+        .rest('quarter');
+
+    leftHand.rest('quarter')
+        .note('quarter', 'Gb3')
+        .note('quarter', 'F3')
+        .rest('quarter');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 9/17
+    rightHand.note('tripletHalf', 'G4, C4')
+        .note('tripletHalf', 'E5, G4')
+        .note('tripletHalf', 'G5, B4');
+
+    leftHand.note('tripletHalf', 'E3')
+        .note('tripletHalf', 'C4')
+        .note('tripletHalf', 'E4');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 10/18
+    rightHand.note('quarter', 'A5, C5')
+        .rest('quarter')
+        .note('quarter', 'F5, A4')
+        .note('quarter', 'G5, B4');
+
+    leftHand.note('quarter', 'F4')
+        .rest('quarter')
+        .note('quarter', 'D4')
+        .note('quarter', 'E4');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 11/19
+    rightHand.rest('quarter')
+        .note('quarter', 'E5, G4')
+        .rest('quarter')
+        .note('quarter', 'C5, E4');
+
+    leftHand.rest('quarter')
+        .note('quarter', 'C4')
+        .rest('quarter')
+        .note('quarter', 'A3');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 12/20
+    rightHand.note('quarter', 'D5, F4')
+        .note('quarter', 'B4, D4')
+        .rest('half');
+
+    // Repeat back to Bar 5
+    rightHand.repeat(1);
+
+    leftHand.note('quarter', 'B3')
+        .note('quarter', 'G3')
+        .rest('half');
+
+    // Repeat back to Bar 5
+    leftHand.repeat(1);
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    drum.repeat(1);
+
+    // Bar 21
+    rightHand.rest('half')
+        .note('quarter', 'G5, E5')
+        .note('quarter', 'Gb5, Eb5');
+
+    leftHand.note('quarter', 'C3')
+        .rest('quarter')
+        .rest('quarter')
+        .note('quarter', 'G3');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 22
+    rightHand.note('quarter', 'F5, D5')
+        .note('quarter', 'D#5, B4')
+        .rest('quarter')
+        .note('quarter', 'E5, C5');
+
+    leftHand.rest('half')
+        .note('quarter', 'C4')
+        .rest('quarter');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 23
+    rightHand.rest('quarter')
+        .note('quarter', 'G#4, E4')
+        .note('quarter', 'A4, F4')
+        .note('quarter', 'C5, A4');
+
+    leftHand.note('quarter', 'F3')
+        .rest('quarter')
+        .rest('quarter')
+        .note('quarter', 'C4');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 24
+    rightHand.rest('quarter')
+        .note('quarter', 'A4, C4')
+        .note('quarter', 'C5, E4')
+        .note('quarter', 'D5, F4');
+
+    leftHand.note('quarter', 'C4')
+        .rest('quarter')
+        .note('quarter', 'F3')
+        .rest('quarter');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 25
+    rightHand.rest('half')
+        .note('quarter', 'G5, E5')
+        .note('quarter', 'Gb5, Eb5');
+
+    leftHand.note('quarter', 'C3')
+        .rest('quarter')
+        .rest('quarter')
+        .note('quarter', 'E3');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 26
+    rightHand.note('quarter', 'F5, D5')
+        .note('quarter', 'D#5, B4')
+        .rest('quarter')
+        .note('quarter', 'E5, C5');
+
+    leftHand.rest('half')
+        .note('quarter', 'G3')
+        .note('quarter', 'C4');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 27
+    rightHand.rest('quarter')
+        .note('quarter', 'C6, G6, F6')
+        .rest('quarter')
+        .note('quarter', 'C6, G6, F6');
+
+    leftHand.rest('whole');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 28
+    rightHand.note('quarter', 'C6, G6, F6')
+        .rest('quarter')
+        .rest('half');
+
+    leftHand.rest('half')
+        .note('quarter', 'G3')
+        .rest('quarter');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 29
+    rightHand.rest('half')
+        .note('quarter', 'G5, E5')
+        .note('quarter', 'Gb5, Eb5');
+
+    leftHand.note('quarter', 'C3')
+        .rest('quarter')
+        .rest('quarter')
+        .note('quarter', 'G3');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 30
+    rightHand.note('quarter', 'F5, D5')
+        .note('quarter', 'D#5, B4')
+        .rest('quarter')
+        .note('quarter', 'E5, C5');
+
+    leftHand.rest('half')
+        .note('quarter', 'C4')
+        .rest('quarter');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 31
+    rightHand.rest('quarter')
+        .note('quarter', 'G#4, E4')
+        .note('quarter', 'A4, F4')
+        .note('quarter', 'C5, A4');
+
+    leftHand.note('quarter', 'F3')
+        .rest('quarter')
+        .rest('quarter')
+        .note('quarter', 'C4');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 32
+    rightHand.rest('quarter')
+        .note('quarter', 'A4, C4')
+        .note('quarter', 'C5, E4')
+        .note('quarter', 'D5, F4');
+
+    leftHand.note('quarter', 'C4')
+        .rest('quarter')
+        .note('quarter', 'F3')
+        .rest('quarter');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 33
+    rightHand.rest('half')
+        .note('quarter', 'Eb5, Ab4')
+        .rest('quarter');
+
+    leftHand.note('quarter', 'C3')
+        .rest('quarter')
+        .note('quarter', 'Ab3')
+        .rest('quarter');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 34
+    rightHand.rest('quarter')
+        .note('quarter', 'D5, F4')
+        .rest('half');
+
+    leftHand.rest('quarter')
+        .note('quarter', 'Bb3')
+        .rest('half');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 35
+    rightHand.note('quarter', 'C5, E4')
+        .rest('quarter')
+        .rest('half');
+
+    leftHand.note('quarter', 'C4')
+        .rest('quarter')
+        .rest('quarter')
+        .note('quarter', 'G3');
+
+    drum.note('eighth')
+        .rest('eighth')
+        .rest('quarter')
+        .note('tripletQuarter')
+        .rest('tripletQuarter')
+        .note('tripletQuarter');
+
+    // Bar 36
+    rightHand.rest('whole');
+
+    leftHand.note('quarter', 'G3')
+        .rest('quarter')
+        .note('quarter', 'C3')
+        .rest('quarter');
+
+    drum.rest('whole');
+
+    rightHand.repeatFromBeginning(500);
+    leftHand.repeatFromBeginning(500);
+    drum.repeatFromBeginning(500);
+
+    var player = conductor.finish();
+
+    $scope.playing = $scope.paused = $scope.muted = false;
+    $scope.volume = 50;
+    $scope.currentSeconds = 0;
+    $scope.timeSlider = 0;
+    $scope.totalSeconds = conductor.getTotalSeconds();
+
+    var pauseTicker = false;
+
+    conductor.setTickerCallback(function(seconds) {
+        
+    });
+
+    conductor.setOnFinishedCallback(function() {
+        
+    });
+
+    conductor.setOnDurationChangeCallback(function() {
+        $scope.totalSeconds = conductor.getTotalSeconds();
+    });
+
+    $scope.play = function() {
+        $scope.playing = true;
+        $scope.paused = false;
+        player.play();
+    };
+
+    $scope.stop = function() {
+        $scope.playing = $scope.paused = false;
+        player.stop();
+    };
+
+    $scope.pause = function() {
+        $scope.paused = true;
+        player.pause();
+    };
+
+    $scope.updateTime = function() {
+        pauseTicker = false;
+        player.setTime($scope.currentSeconds);
+    };
+    
+    play();
+};
+
+startAudio();
