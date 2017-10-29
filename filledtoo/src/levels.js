bcba42a79ea13451f6170fa29d7e513cf11a92ab
bauwen <bauwen.demol@hotmail.com>
2017-10-29 10:56:57 +0100

    extra levels

--- filledtoo/src/levels.js
+++ filledtoo/src/levels.js
@@ -405,6 +405,472 @@ var LEVELS = [
     ".5...........@5." +
     "oooooooooooooooo",
     
+    // ----------------- 40 --------------------- (extra levels)
+    
+    /*
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    */
+    
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "ooooo@oooooooooo" +
+    "ooooo...21.ooooo" +
+    "ooooo..325.ooooo" +
+    "ooooo.31#5.ooooo" +
+    "ooooo..44.xooooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    
+    // gif out
+    "oooooooooooooooo" +
+    "........21......" +
+    ".#####..##.####." +
+    ".##54....2.####." +
+    ".##.###.##.##@.." +
+    ".##.x##.##.###1." +
+    "5######.##.##..." +
+    "3.....4..3......" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "oo............oo" +
+    "o.....###......o" +
+    "..#...##..##...@" +
+    "..##...#..###..." +
+    "o..#...##......." +
+    "oo....###.....xo" +
+    "oo..o.........oo" +
+    "oooooooooooooooo",
+    
+    
+    "oooooooooooooooo" +
+    "oo...ooooooo..oo" +
+    "oo....ooooo...oo" +
+    "ooo.x.....o.#.oo" +
+    "ooo...%%....@.oo" +
+    "oo...o..o..o..oo" +
+    "oo.#.o..o..o..oo" +
+    "oo...o..o..o..oo" +
+    "oooooooooooooooo",
+    
+    
+    "oooooooooooooooo" +
+    "ooo..........ooo" +
+    "oo...........xoo" +
+    "oo...oo.oo....oo" +
+    "ooooooo.oooooooo" +
+    "oooo...%....oooo" +
+    "oo............oo" +
+    "oo.....@......oo" +
+    "oooooooooooooooo",
+    
+    /*
+    "oooooooooooooooo" +
+    "oooo........oooo" +
+    "ooo.........oooo" +
+    "oo..#...#..#..oo" +
+    "oo..#..##..#..oo" +
+    "oo@.#..#...#..oo" +
+    "ooox........%.oo" +
+    "ooo..........ooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "o..xo....o.....@" +
+    "...%o..%%%%....." +
+    "..o%o...o..oooo." +
+    "..o%oooooooo..o." +
+    "..o%%%%%%%%%%.o." +
+    "..ooooooooooooo." +
+    "................" +
+    "oooooooooooooooo",
+    */
+    
+    "oooooooooooooooo" +
+    "oo.......oo...oo" +
+    "oo..#..#.oo...oo" +
+    "oo..@....o...ooo" +
+    "oo.......o.x%.oo" +
+    "oo...#...o....oo" +
+    "oo.#..%..oo...oo" +
+    "oo.....o......oo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "ooo..x..oo1.oooo" +
+    "ooo.##.....%.ooo" +
+    "ooo.#@.......ooo" +
+    "ooo..%..##...ooo" +
+    "ooo...#......ooo" +
+    "ooo...1o....oooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    
+    // A
+    "oooooooooooooooo" +
+    "ooo.......ox.ooo" +
+    "ooo.##..1.2#.ooo" +
+    "ooo.2..###...ooo" +
+    "ooo.#..##1.#@ooo" +
+    "ooo.......#..ooo" +
+    "ooo.........oooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    /*
+    "oooooooooooooooo" +
+    "oo..o...o.....oo" +
+    "oo.2o......o1.oo" +
+    "oo.oo..#...ooooo" +
+    "oo.....@x.....oo" +
+    "oooo....#..oo.oo" +
+    "oo.1###...oo2.oo" +
+    "oo........oo..oo" +
+    "oooooooooooooooo",
+    */
+    
+    // A
+    "oooooooooooooooo" +
+    "...............o" +
+    "..........#....o" +
+    "o....#x.#......o" +
+    "o...##..##..#..o" +
+    "..#..#.@#...#..." +
+    "............#..." +
+    "o..............." +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "oo.....oo....@oo" +
+    "oo...#........oo" +
+    "oo...#.....#..oo" +
+    "ooo....#.....ooo" +
+    "oo..#.....#...oo" +
+    "oo...%........oo" +
+    "oox...oo.....ooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "o.............1o" +
+    "o.#............o" +
+    "o.#..###@.#....o" +
+    "o..............o" +
+    "o....##..##..#.o" +
+    "o...........##.o" +
+    "ox1............o" +
+    "oooooooooooooooo",
+    
+    // 52
+    "oooooooooooooooo" +
+    "%%..o.x.......%%" +
+    "%.#..........#.%" +
+    ".##....###...##." +
+    ".1....####....1." +
+    ".##...#@.....##." +
+    "%.#.........##.%" +
+    "%%.......oo...%%" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "....o..........." +
+    "............#..." +
+    "......#........." +
+    "....1..........o" +
+    ".#.....#.....ooo" +
+    ".....#....#..ox." +
+    "..o.........@o1." +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "....o......o...2" +
+    ".#............#3" +
+    ".#.....##.....#." +
+    ".#..#..11..#..#." +
+    ".#.....##.....#." +
+    "3##....@.....x#." +
+    "2...o......o...." +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "2.@............1" +
+    ".....x......4..." +
+    ".....3.........." +
+    "................" +
+    "................" +
+    "....4....3......" +
+    "1..............2" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "o......o..o....." +
+    "....##.o........" +
+    "..#....o........" +
+    "o.x....o......#1" +
+    "o....#.o...#...o" +
+    "1##..#.o........" +
+    ".......oo..@o..." +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oo%%........%%oo" +
+    "oo%.#...%%.#%%oo" +
+    "oo..#..%%..#..oo" +
+    "oo%%#..%%.x#%%oo" +
+    "oo%%....@...%.oo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "oo............oo" +
+    "oo.#........#.oo" +
+    "oo...#....#...oo" +
+    "oo..##....##..oo" +
+    "oo..x.........oo" +
+    "oo@....##.....oo" +
+    "oo............oo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "2..............." +
+    ".x.....#3..##..." +
+    "...3......%.1..." +
+    "..#......#......" +
+    "....1..@.......2" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "o............o@o" +
+    "o.#..#.........o" +
+    "o..##....#..1..o" +
+    "o........###...o" +
+    "o..........#...o" +
+    "o1..xo.........o" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "o@.....oo....1.o" +
+    "o....x.........o" +
+    "ooooo..#.%.....o" +
+    "o..............o" +
+    "o.##..#....#...o" +
+    "o..........1...o" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "2.x..........o.." +
+    "..1......#......" +
+    "...%.#..%.#@.##." +
+    "..2..#........1." +
+    ".#............#." +
+    "................" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oo@o...2...o1ooo" +
+    "oo.3..1..#43.ooo" +
+    "ooo5%%.....#4ooo" +
+    "oox2.5.......ooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "...........o...." +
+    "...#............" +
+    ".........#......" +
+    "o..........x...." +
+    "........#.....#." +
+    ".....#....#....." +
+    "@............o.." +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "o..o........oooo" +
+    "o.......#......." +
+    "o..##..%.%..%..." +
+    ".....%..#.##...o" +
+    "......#..%...%.o" +
+    ".....%......#..o" +
+    "ooo@ox.........o" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    ".3..4..o.....2o@" +
+    "..........#....." +
+    "...##.......#4.o" +
+    ".....#...#......" +
+    "1......#......1." +
+    "...2.......3...." +
+    "xo....oooo......" +
+    "oooooooooooooooo",
+    
+    // 67
+    "oooooooooooooooo" +
+    "ox............2o" +
+    "o..........1...o" +
+    "o.....#........o" +
+    "o..#...%...%...o" +
+    "o.%...%%....#..o" +
+    "o@...#....#....o" +
+    "o2.1...........o" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oo.....1......@o" +
+    "o..x.......##..o" +
+    "o.##....#2.##..o" +
+    "o...##2...%....o" +
+    "o............1.o" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "o..1...........o" +
+    "o......#3......o" +
+    "o.2..#4..%.#...o" +
+    "o.....3.....2..o" +
+    "ox......4...o..o" +
+    "o..o....o..1oo@o" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "...............o" +
+    ".#....#........o" +
+    "......#...#...%." +
+    "..x#....#....#.." +
+    ".#............%." +
+    "....##....ooo..." +
+    "o..@......ooo..." +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "@..............." +
+    "..##3....5..###." +
+    "..#1..4..2...1#." +
+    "..#...#5......#." +
+    "......#x##2....." +
+    ".######....3.4.." +
+    "................" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "4......5.......1" +
+    "5.%.####...###.." +
+    "...#...3........" +
+    "..#....#..@..#1." +
+    "..#..4#..3....#2" +
+    ".....##...###2.." +
+    "..oo..x........o" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "ooo.......22.ooo" +
+    "ooo.#@3.55...ooo" +
+    "ooo...3..x#..ooo" +
+    "ooo..........ooo" +
+    "ooo...11..44.ooo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "o.............@o" +
+    "o..%%%.%%%.%%%.o" +
+    "o..%%%.%.%.%.%.o" +
+    "o...%%.%%%.%%%.o" +
+    "ox.............o" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "@..............." +
+    "..1............." +
+    ".......##....#.." +
+    "....##.........." +
+    "..#.......#....." +
+    "................" +
+    "...xo..o.......1" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    ".2oooo.........." +
+    "...3.......###1." +
+    "3.#2.####..1...." +
+    "o.#..###.%......" +
+    "o.#x......#####." +
+    "o......%...@###." +
+    "o....oo........." +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "oooooooooooooooo" +
+    "oo..x2.......3oo" +
+    "oo..1......1..oo" +
+    "oo.....@......oo" +
+    "oo...#...2..#.oo" +
+    "oo3...........oo" +
+    "oooooooooooooooo" +
+    "oooooooooooooooo",
+    
+    "oooooooooooooooo" +
+    "................" +
+    "....2..........1" +
+    "....5..........." +
+    "............%..." +
+    "......@..5.2...." +
+    ".....1........4." +
+    ".3.....4......3x" +
+    "oooooooooooooooo",
+    
+    
+    // extra
+    
+    
+    
+    "oooooooooooooooo" +
+    "ooooo@.....ooooo" +
+    "ooooo......ooooo" +
+    "ooooo..21.3ooooo" +
+    "ooooo..15.4ooooo" +
+    "ooooo..35..ooooo" +
+    "ooooo.%x2..ooooo" +
+    "ooooo4.....ooooo" +
+    "oooooooooooooooo",
+    
+    
+    
+    
+    // gif in
+    "oooooooooooooooo" +
+    "....@oo13oo....x" +
+    "..ooooooooo..ooo" +
+    "..ooooo..oo4.ooo" +
+    "..o2.1o5.oo...4o" +
+    "..oo5oo..oo..ooo" +
+    ".%%..oo..oo..ooo" +
+    ".%...oo3.oo.2ooo" +
+    "oooooooooooooooo",
+    
     "oooooooooooooooo" +
     "oooooooooooooooo" +
     "oooooooooooooooo" +
